a39e81062cb7dc6f4b5e2c9b40d5bdf87e8494e5
Abelardo Miranda <available_email_address@yahoo.com>
2020-05-24 01:59:41 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -8,9 +8,7 @@ def introduction_with_language_optional(name, language="Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction
-introduction_with_language(Abel, Javascript)
-introduction_with_language_optional(Abel)
+
 
 
 
